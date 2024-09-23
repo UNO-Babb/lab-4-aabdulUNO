fa9ad7855cd3bd2ec3bd03e3c3205539c691a529
aabdulUNO <ayubabdulx@gmail.com>
2024-09-22 21:53:44 -0600

    Update TurtleGraphics.py

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,27 +1,100 @@
 #TurtleGraphics.py
-#Name:
+#Name:Ayub
 #Date:
 #Assignment:
 
-import turtle #needed generally but not in CodeHS
-hideturtle() #hides the default turtle in CodeHS
+import turtle 
+
+Ayub = turtle.Turtle
 
 def drawSquare(myTurtle, size):
     for i in range(4):
-        myTurtle.forward(size)
+        myTurtle.forward(200)
         myTurtle.right(90)
 
 
 def main():
-    myTurtle = turtle.Turtle()
+    Ayub = turtle.Turtle()
     # drawPolygon(myTurtle, 5) #draws a pentagon
+    for _ in range (sides):
+        t.foward(100) # Move foward by 100 units
+        t.right(angle) # turn right by the calculated angle
+
+
     # drawPolygon(myTurtle, 8) #draws an octogon
+      def draw_octagon();
+        for _ in range(8)
+          octagon_turtle.foward(100)
+          octagon_turtle.left(45)
 
     # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
+        for_in range(4):
+          t.foward(100
+          t.right(90)
+    # Fill in the top right corner
+        t.fillcolor("Purple") # Change the fill color as needed
+        t.begin_fill()
+    # Move to the top right corner
+      t.penup()
+      t.goto(100,100)
+      t.pendown()
+
+      t.end_fill()
+
     # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
+    for_in range(4):
+          t.foward(100
+          t.right(90)
+    # function to fill the bottom left corner
+    def fill_bottom_left(size):
+        turtle.begin_fill(200):
+        turtle.goto(0,0)
+        turtle.goto(0,-200)
+        turtle.goto(-size,-200)
+        turtle.goto(-200,0)
+        turtle.end_fill()
+
+  Turtle.speed(1)
+  turtle.penup()
+  Turtle.goto(0,0)
+  turtle.pendown()
+
+
+turtle.fillcolor("Orange")
+fill_bottom_left(100)
+
+  turtle.penup()
+  Turtle.goto(0,0)
+  turtle.pendown()
+  draw_square(100)
+
+turtle.hideturtle()
+turtle.done()
+                   
 
     # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
+     def drawSquare(myTurtle, size):
+    for i in range(5):
+        draw_square(100 + i*20)
+        t.penup()
+        t.right(90)
+        t.foward(18)
+        t.left(90)
+        t.pendown()
+        
+         
     # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
+    def drawSquare(myTurtle, size):
+    for i in range(4):
+        myTurtle.forward(size)
+        myTurtle.right(90)
+    size + [100, 150, 200]
+    for size in sizes:
+        t.penup()
+        t.right(90)
+        t.foward(25)
+        t.left(90)
+        t.pendown()
 
-
+turtle.done()
 main()
